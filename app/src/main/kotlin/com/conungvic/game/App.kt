563a56310a652fb97e082a9a4320c66f07665239
Igor <conung.vic@gmail.com>
2022-03-12 18:30:57 +0200

    Fix issue with non-latin user names

--- app/src/main/kotlin/com/conungvic/game/App.kt
+++ app/src/main/kotlin/com/conungvic/game/App.kt
@@ -7,6 +7,8 @@ package com.conungvic.game
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
 
 
 fun main() {
@@ -15,6 +17,28 @@ fun main() {
     config.height = Config.prop.get(HEIGHT).toInt()
     config.title = "Knight"
     config.resizable = true
+    checkIsUsernameNotLatin()
 //    config.fullscreen = true
     LwjglApplication(KnightGame(), config)
+}
+
+fun checkIsUsernameNotLatin() {
+    val name = System.getProperty("user.name")
+    if (!isPureAscii(name)) {
+        val buf = StandardCharsets.UTF_8.encode(name)
+        val sBuf = StringBuffer()
+        for (b in buf.array()) {
+            if (1*b != 0) {
+                val hex = Integer.toHexString(1 * b).substring(6, 8).uppercase()
+                sBuf.append("\\x$hex")
+            }
+        }
+        System.setProperty("user.name", sBuf.toString())
+    }
+}
+
+fun isPureAscii(v: String): Boolean {
+    return Charset.forName(StandardCharsets.ISO_8859_1.name()).newEncoder().canEncode(v)
+    // or "ISO-8859-1" for ISO Latin 1
+    // or StandardCharsets.US_ASCII with JDK1.7+
 }
